a35f79597290196865055962b77a756c79181b6a
trottling <truewasted@yandex.ru>
2024-02-17 15:05:25 +0300

    improve logger

--- OnAppStart/setup_logger.py
+++ OnAppStart/setup_logger.py
@@ -3,17 +3,18 @@ import logging
 import platform
 import sys
 import time
-from logging import StreamHandler
+from logging import StreamHandler, Formatter
 
 
 def Setup_logger(app_version, file_handler):
     logger = logging.getLogger(__name__)
     std_handler = StreamHandler(sys.stdout)
+    std_handler.setFormatter(Formatter("%(asctime)s %(levelname)s %(message)s"))
+    file_handler.setFormatter(Formatter("%(asctime)s %(levelname)s %(message)s"))
     logger.addHandler(std_handler)
     logger.addHandler(file_handler)
     logger.setLevel(logging.DEBUG)
 
-    logger.debug(time.strftime("%Y-%m-%d | %H:%M:%S", time.localtime()))
     logger.debug(f"Python {sys.version}")
     logger.debug(f"Application version: {app_version}")
     logger.debug(f"Run as Admin : {Check_Admin(logger)}")
