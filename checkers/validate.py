83959ea9a79208877a515ecc7d89760300e94027
trottling <truewasted@yandex.ru>
2024-02-19 14:49:57 +0300

    improve err message

--- checkers/validate.py
+++ checkers/validate.py
@@ -7,14 +7,15 @@ def Validate_before_check(self):
             ShowErrMessage(self, "Vulners.com API key is empty")
             return True
         if not self.isVulnersKeyValid:
-            ShowErrMessage(self, "Check and validate vulners.com API key before start")
+            ShowErrMessage(self, "Check and validate vulners.com API key in settings before start")
             return True
         self.logger.debug("Validate_before_check : All normal")
         return False
 
 
 def ShowErrMessage(self, msg):
-    self.ui.alert_msg.show()
-    self.ui.alert_msg.setText(msg)
-    self.logger.debug(f"ShowErrMessage : {msg}")
-    QTimer.singleShot(5000, lambda: self.ui.alert_msg.hide())
+    if not self.ui.alert_msg.isVisible():
+        self.ui.alert_msg.show()
+        self.ui.alert_msg.setText(msg)
+        self.logger.debug(f"ShowErrMessage : {msg}")
+        QTimer.singleShot(5000, lambda: self.ui.alert_msg.hide())
