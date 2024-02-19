270d0289a57333dcc77d28efcabf44df9b439277
trottling <truewasted@yandex.ru>
2024-02-19 14:31:07 +0300

    done writing app checker

--- checkers/Check_Installed_Apps.py
+++ checkers/Check_Installed_Apps.py
@@ -1,27 +1,70 @@
+from vulners import vscanner
 from windows_tools.installed_software import get_installed_software
-from ui.tools import Report_Error
+import vulners
+import json
+from checkers.rep import rep
 
 
 def RunCIA(self):
-    raw_soft_list = []
-    soft_list = []
+    self.raw_soft_list = []
+    self.soft_list = []
+    self.soft_list_vulners = []
+    self.report = None
+
     self.logger.debug("RunCIA : Started")
-    self.ui.stackedWidget.setCurrentIndex(1)
-    self.ui.work_log.appendPlainText("Check installed apps started..." + "\n")
+    self.log_signal.emit("Check installed apps started..." + "\n")
 
     try:
-        raw_soft_list = get_installed_software()
+        self.raw_soft_list = get_installed_software()
     except Exception as e:
-        Report_Error(self, e)
+        self.err_signal.emit("Error getting installed apps : " + str(e))
+        return
 
-    self.logger.debug(f"RunCIA : Raw Soft list : {raw_soft_list}")
+    self.logger.debug(f"RunCIA : Raw Soft list : {len(self.raw_soft_list)} softwares")
 
-    for software in raw_soft_list:
-        if software['name'] != "" and software['version'] != "" and software['publisher'] != "":
-            soft_list.append(software)
+    for software in self.raw_soft_list:
+        if software['name'] != "" and software['version'] != "":
+            self.soft_list.append(software)
 
-    self.logger.debug(f"RunCIA : Soft list : {soft_list}")
+    if len(self.soft_list) == 0:
+        self.err_signal.emit("ERROR : Could not find any software to check")
 
-    self.ui.progressBar.setValue(10)
+    self.logger.debug(f"RunCIA : Soft list : {len(self.soft_list)} softwares")
+    self.log_signal.emit(f"Found {len(self.raw_soft_list)} softwares, {len(self.soft_list)} aviable to check\n")
 
-    self.ui.work_log.appendPlainText(f"Found {len(soft_list)} softwares\n")
+    match self.db:
+        case "vulners.com (Recommended)": Check_By_Vulners(self)
+
+
+def Check_By_Vulners(self):
+    if len(self.soft_list) > 500:
+        self.soft_list = self.soft_list[:500]
+        self.log_signal.emit(f"Software list is too bigger : {len(self.soft_list)} : Cut lenght to 500\n")
+
+    self.log_signal.emit(f"Transforming softwares list to vulners format\n")
+    for software in self.soft_list:
+        if self.db == "vulners.com (Recommended)":
+            self.soft_list_vulners.append(dict({"software": software['name'], "version": software['version']}))
+    """
+    self.log_signal.emit(f"Connecting to vulners API\n")
+    try:
+        self.vulners_api = vulners.VulnersApi(api_key=self.api_key)
+    except Exception as e:
+        self.err_signal.emit("Failed to connect Vulners Api : " + str(e))
+        return
+
+    self.log_signal.emit(f"Sending softwares list to vulners\n")
+    try:
+        self.report = self.vulners_api.software_audit(os="", version="", packages=self.soft_list_vulners)
+    except Exception as e:
+        self.err_signal.emit("Failed to get Vulners report : " + str(e))
+        return
+        """
+    self.report = rep
+    try:
+        self.log_signal.emit(
+            f"Done : {len(self.report.get("exploit"))} Exploits : {len(self.report.get("software"))} Software vulnerabiliries\n")
+    except Exception as e:
+        self.err_signal.emit("Failed read Vulners report : " + str(e))
+    self.log_signal.emit("Click Next button to see scan results")
+    self.result_signal.emit(self.report)
