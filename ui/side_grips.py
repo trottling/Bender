18a73733ac26ee0fcfad0837bf0be7305dba8485
trottling <truewasted@yandex.ru>
2024-03-02 21:50:16 +0300

    add side grips

---
+++ ui/side_grips.py
@@ -0,0 +1,55 @@
+from PyQt6 import QtCore, QtWidgets
+
+
+class SideGrip(QtWidgets.QWidget):
+    def __init__(self, parent, edge):
+        QtWidgets.QWidget.__init__(self, parent)
+        if edge == QtCore.Qt.Edge.LeftEdge:
+            self.setCursor(QtCore.Qt.CursorShape.SizeHorCursor)
+            self.resizeFunc = self.resizeLeft
+        elif edge == QtCore.Qt.Edge.TopEdge:
+            self.setCursor(QtCore.Qt.CursorShape.SizeVerCursor)
+            self.resizeFunc = self.resizeTop
+        elif edge == QtCore.Qt.Edge.RightEdge:
+            self.setCursor(QtCore.Qt.CursorShape.SizeHorCursor)
+            self.resizeFunc = self.resizeRight
+        else:
+            self.setCursor(QtCore.Qt.CursorShape.SizeVerCursor)
+            self.resizeFunc = self.resizeBottom
+        self.mousePos = None
+
+    def resizeLeft(self, delta):
+        window = self.window()
+        width = max(window.minimumWidth(), window.width() - delta.x())
+        geo = window.geometry()
+        geo.setLeft(geo.right() - width)
+        window.setGeometry(geo)
+
+    def resizeTop(self, delta):
+        window = self.window()
+        height = max(window.minimumHeight(), window.height() - delta.y())
+        geo = window.geometry()
+        geo.setTop(geo.bottom() - height)
+        window.setGeometry(geo)
+
+    def resizeRight(self, delta):
+        window = self.window()
+        width = max(window.minimumWidth(), window.width() + delta.x())
+        window.resize(width, window.height())
+
+    def resizeBottom(self, delta):
+        window = self.window()
+        height = max(window.minimumHeight(), window.height() + delta.y())
+        window.resize(window.width(), height)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.MouseButton.LeftButton:
+            self.mousePos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if self.mousePos is not None:
+            delta = event.pos() - self.mousePos
+            self.resizeFunc(delta)
+
+    def mouseReleaseEvent(self, event):
+        self.mousePos = None
