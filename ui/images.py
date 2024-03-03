307971e1855581665dafb2b30dc3825c58871432
trottling <truewasted@yandex.ru>
2024-03-03 10:39:49 +0300

    remove raw string prefix

--- ui/images.py
+++ ui/images.py
@@ -10,7 +10,7 @@ def Load_Images_And_Icons(self):
     self.ui.logo_3.setStyleSheet(
         ".QFrame {border-image: url('" + GetRelPath(self, 'assets//images//bender.png') + "')}")
     self.ui.git_frame.setStyleSheet(
-        r".QFrame {image: url('" + GetRelPath(self, 'assets//images//github.png') + "')}")
+        ".QFrame {image: url('" + GetRelPath(self, 'assets//images//github.png') + "')}")
 
     self.logger.debug(f"Load_Images_And_Icons : Logos seted")
 
@@ -18,62 +18,62 @@ def Load_Images_And_Icons(self):
 
     # Toolbar
     self.ui.pushButton_app_exit.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//exit.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//exit.png') + "')}")
+    self.ui.pushButton_app_size.setStyleSheet(
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//resize.png') + "')}")
     self.ui.pushButton_app_hide.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//minimize.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//minimize.png') + "')}")
     self.ui.app_icon.setStyleSheet(
-        r".QFrame {image: url('" + GetRelPath(self, 'assets//images//bender-small.png') + "')}")
+        ".QFrame {image: url('" + GetRelPath(self, 'assets//images//bender-small.png') + "')}")
 
     # Settings
     self.ui.setting_back_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
     self.ui.lang_apply_pushButton.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//apply.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//apply.png') + "')}")
     self.ui.qss_apply_pushButton.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//apply.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//apply.png') + "')}")
     self.ui.reset_qss_pushButton.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//reset.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//reset.png') + "')}")
     self.ui.qss_apply_file_pushButton.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//add.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//add.png') + "')}")
     self.ui.qss_file_pushButton.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//qss-file.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//qss-file.png') + "')}")
     self.ui.check_key_pushButton.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//check.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//check.png') + "')}")
     self.ui.delete_qss_pushButton.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//delete.png') + "')}")
-    self.ui.vulners_key_help.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//help.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//delete.png') + "')}")
 
     # Pages
 
     # Start page
     self.ui.setting_btn.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//settings.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//settings.png') + "')}")
     self.ui.info_btn.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//info.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//info.png') + "')}")
 
     # Work log page
     self.ui.back_work_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
     self.ui.next_work_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//next.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//next.png') + "')}")
 
     # Work result page
     self.ui.back_result_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
     self.ui.next_result_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//fail.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//fail.png') + "')}")
 
     # Info page
     self.ui.info_back_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
 
     # CVE info page
     self.ui.cve_info_back_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
 
     # Vuln info page
     self.ui.vuln_info_back_button.setStyleSheet(
-        r".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
+        ".QPushButton {image: url('" + GetRelPath(self, 'assets//images//back.png') + "')}")
 
     self.logger.debug(f"Load_Images_And_Icons : Buttons icons seted")
