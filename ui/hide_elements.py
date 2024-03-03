a26a9b6738ec00cce53e1302ad72c430368292d8
trottling <truewasted@yandex.ru>
2024-03-03 10:33:10 +0300

    remove cve db selector

--- ui/hide_elements.py
+++ ui/hide_elements.py
@@ -8,13 +8,5 @@ def Hide_Elements(self):
     self.ui.delete_qss_pushButton.hide()
     self.ui.label_windows_title.hide()
     self.ui.app_icon.hide()
-    self.ui.label_no_cve.hide()
-
-    if self.ui.db_comboBox.currentText() != "vulners.com (Recommended)":
-        self.ui.label_vulners_key.hide()
-        self.ui.api_key.hide()
-        self.ui.check_key_pushButton.hide()
-        self.ui.vulners_check_result.hide()
-        self.ui.vulners_key_help.hide()
 
     self.logger.debug("Hide_Elements : Elements Hided")
