3d1ae70f1959f8a9ca2cda3d99ae1ec26ce69f84
trottling <truewasted@yandex.ru>
2024-03-07 11:23:11 +0300

    improve UI

--- ui/hide_elements.py
+++ ui/hide_elements.py
@@ -1,13 +1,16 @@
+from ui.animations import ElemHideAnim
 from ui.buttons import HideQSSInput
 
 
 def Hide_Elements(self):
     self.ui.alert_msg.hide()
-    self.ui.next_work_btn.hide()
     self.ui.delete_qss_pushButton.hide()
     self.ui.label_windows_title.hide()
     self.ui.app_icon.hide()
 
     HideQSSInput(self)
+
+    ElemHideAnim(self, self.ui.next_work_btn, hide=False)  # Just set opacity to 0
+    self.ui.next_work_btn.setEnabled(False)  # And make inactive
 
     self.logger.debug("Hide_Elements : Elements Hided")
