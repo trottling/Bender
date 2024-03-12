1dc635dbab14a91a8a1ca34a150bab1a8a99393a
trottling <truewasted@yandex.ru>
2024-03-12 16:18:34 +0300

    add log path to log

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app_version = "1.0.0"
 # Check App folder
 appdir, file_handler = CheckAppDir()
 # Setup logger
-logger = Setup_logger(app_version, file_handler)
+logger = Setup_logger(app_version, file_handler, appdir)
 
 if __name__ == '__main__':
     # Run GUI
