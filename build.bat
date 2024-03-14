2e5c2bbb9e092dda9d109a495100e3bfb3295d55
trottling <truewasted@yandex.ru>
2024-03-14 09:14:00 +0300

    update build

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 echo Setup packages
 pip install -r requirements.txt
 
-echo Building --noconsole
-pyinstaller --name "Bender" --clean --onefile --noupx --noconsole --uac-admin --add-data "assets\gifs\*.*:assets\gifs" --add-data "assets\qss\*.*:assets\qss" --add-data "assets\icons\*.*:assets\icons"--add-data "assets\images\*.*:assets\images" --add-data "assets\app.ui:assets" --paths C:\Windows\System32\downlevel --icon ".\assets\icons\bender.ico" main.py
+echo Building
+pyinstaller --name "Bender" --clean --onefile --noconsole --uac-admin --splash "assets\images\splash.png" --upx-dir "C:\Users\truew\PycharmProjects\upx-4.2.2-win64\upx.exe" --add-data "assets\gifs\*.*:assets\gifs" --add-data "assets\qss\*.*:assets\qss" --add-data "assets\icons\*.*:assets\icons"--add-data "assets\images\*.*:assets\images" --add-data "assets\app.ui:assets" --paths C:\Windows\System32\downlevel --icon ".\assets\icons\bender.ico" main.py
 move "dist\main.exe" "main.exe"
 
 del main.spec
