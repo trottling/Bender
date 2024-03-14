c55c476ba3094f4dc00a6499870bdf39fc1d8aff
trottling <truewasted@yandex.ru>
2024-03-14 08:51:25 +0300

    add gifs folder to build

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ echo Setup packages
 pip install -r requirements.txt
 
 echo Building --noconsole
-pyinstaller --name "Bender" --clean --onefile --noupx --noconsole --uac-admin --add-data "assets\qss\*.*:assets\qss" --add-data "assets\icons\*.*:assets\icons"--add-data "assets\images\*.*:assets\images" --add-data "assets\app.ui:assets" --paths C:\Windows\System32\downlevel --icon ".\assets\icons\bender.ico" main.py
+pyinstaller --name "Bender" --clean --onefile --noupx --noconsole --uac-admin --add-data "assets\gifs\*.*:assets\gifs" --add-data "assets\qss\*.*:assets\qss" --add-data "assets\icons\*.*:assets\icons"--add-data "assets\images\*.*:assets\images" --add-data "assets\app.ui:assets" --paths C:\Windows\System32\downlevel --icon ".\assets\icons\bender.ico" main.py
 move "dist\main.exe" "main.exe"
 
 del main.spec
