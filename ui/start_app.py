2d4fe3f1719a07eb1b577f7f9ae75a5bd8369860
trottling <truewasted@yandex.ru>
2024-03-16 12:35:22 +0300

    move Start_App to start_app.py

---
+++ ui/start_app.py
@@ -0,0 +1,41 @@
+from config.read_config import Load_Settings
+from tasks.start_tasks import Run_Start_Tasks
+from ui.animations import App_Open_Anim
+from ui.buttons import Connect_Buttons
+from ui.hide_elements import Hide_Elements
+from ui.images import Load_Images_And_Icons
+from ui.prepare_window import Prepare_Window
+from ui.splash import SplashText, StopSplash
+from ui.styles import Load_Styles
+
+
+def Start_App(self) -> None:
+    # Setup Splash screen
+    SplashText(self)
+
+    # Anywhere shit
+    Prepare_Window(self)
+
+    # Load settings
+    Load_Settings(self)
+
+    # Hide elements
+    Hide_Elements(self)
+
+    # Connect buttons
+    Connect_Buttons(self)
+
+    # Load images and Icons
+    Load_Images_And_Icons(self)
+
+    # Load styles
+    Load_Styles(self)
+
+    # Break Splash screen
+    StopSplash(self)
+
+    # Show UI
+    App_Open_Anim(self)
+
+    # Run start tasks
+    Run_Start_Tasks(self)
