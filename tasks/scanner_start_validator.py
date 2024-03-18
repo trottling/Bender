3248432f8296f41b554cf8232ee1a9341af72136
trottling <truewasted@yandex.ru>
2024-03-18 16:20:08 +0300

    remove ports range

--- tasks/scanner_start_validator.py
+++ tasks/scanner_start_validator.py
@@ -37,22 +37,6 @@ def StartScannerValidator(self):
             ShowErrMessage(self, "Loldrivers.io Unavailable, try run scanner later")
             return True
 
-        self.ports_range = self.ui.lineEdit__port_range.text()
-        check_range = self.ports_range.split(',')
-        port_list = []
-        for port in check_range:
-            if re.match(r'^\d+$', port):
-                port_list.append(int(port))
-            elif re.match(r'^\d+-\d+$', port):
-                p_start = int(port.split('-')[0])
-                p_end = int(port.split('-')[1])
-                p_range = list(range(p_start, p_end + 1))
-                port_list.extend(p_range)
-        if len(port_list) == 0:
-            self.ports_range = "22, 23, 80, 443"
-
-        self.logger.debug(f"StartScannerValidator : ports range : {self.ui.lineEdit__port_range.text()} --> {self.ports_range}")
-
         self.logger.debug("StartScannerValidator : All normal")
 
     except Exception as e:
