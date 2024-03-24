849727b093cbd187a72b42948c1f521bd745512a
trottling <truewasted@yandex.ru>
2024-03-24 20:17:33 +0300

    fix instance check

--- on_start/check_instance.py
+++ on_start/check_instance.py
@@ -1,9 +1,9 @@
+import os
 import sys
+
 import wmi
 
 
 def Check_Instance():
-    if sys.argv[0] in wmi.WMI().Win32_Process():
+    if os.path.basename(sys.argv[0]) in [process.Name for process in wmi.WMI().Win32_Process()]:
         sys.exit(-1)
-
-
