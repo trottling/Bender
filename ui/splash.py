e22ccceed0dd91e15b77509e1802bc70f442f5a2
trottling <truewasted@yandex.ru>
2024-03-25 14:32:48 +0300

    improve splash

--- ui/splash.py
+++ ui/splash.py
@@ -1,7 +1,7 @@
 from PyQt6 import uic, QtTest
-from PyQt6.QtCore import Qt, QPropertyAnimation
+from PyQt6.QtCore import Qt, QPropertyAnimation, QEasingCurve
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QSplashScreen
+from PyQt6.QtWidgets import QSplashScreen, QGraphicsOpacityEffect
 
 from ui.tools import GetRelPath
 
@@ -9,6 +9,7 @@ from ui.tools import GetRelPath
 class SplashScreen(QSplashScreen):
     def __init__(self, ):
         super(QSplashScreen, self).__init__()
+        self.info_label = None
         self.progressBar = None
 
         uic.loadUi(GetRelPath(self, "assets/ui/splash.ui"), self)
@@ -16,10 +17,14 @@ class SplashScreen(QSplashScreen):
         self.setWindowTitle("Bender | Loading...")
         self.setPixmap(QPixmap(GetRelPath(self, "assets/images/splash.png")))
 
+        self.effect = QGraphicsOpacityEffect(self.info_label)
+        self.effect.setOpacity(1.0)
+        self.info_label.setGraphicsEffect(self.effect)
+
     def ChangePbar(self, percent: int, text: str):
         self.PercAnim(percent)
-        self.progressBar.setFormat(text)
-        QtTest.QTest.qWait(200)
+        self.TextAnim(text)
+        QtTest.QTest.qWait(150)
 
     def PercAnim(self, percent: int):
         animation = QPropertyAnimation(self.progressBar, b"value", self)
@@ -27,3 +32,24 @@ class SplashScreen(QSplashScreen):
         animation.setStartValue(self.progressBar.value())
         animation.setEndValue(percent)
         animation.start()
+
+    def TextAnim(self, text: str):
+        # Hide
+        anim = QPropertyAnimation(self.effect, b"opacity", self)
+        anim.setDuration(100)
+        anim.setStartValue(self.effect.opacity())
+        anim.setEndValue(0.0)
+        anim.setEasingCurve(QEasingCurve.Type.OutQuad)
+        anim.start()
+        QtTest.QTest.qWait(100)
+
+        # Change Text
+        self.info_label.setText(text)
+
+        # Show
+        anim = QPropertyAnimation(self.effect, b"opacity", self)
+        anim.setDuration(100)
+        anim.setStartValue(self.effect.opacity())
+        anim.setEndValue(1.0)
+        anim.setEasingCurve(QEasingCurve.Type.OutQuad)
+        anim.start()
